46a4c4fb9e8d6a1ef238046d80d13d1886b0e93f
meganlkm <megan.lkm@gmail.com>
2014-07-06 12:00:37 -0500

    shell snippets

---
+++ shell/text_replace.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# javafy regex
+REGEX='/^\d{5}\\(?:[-\s]\d{4})?$/'
+sed 's/\\/\\\\/g' <<< $REGEX
+
+echo '/^\d{5}\\(?:[-\s]\d{4})?$/' | sed 's/\\/\\\\/g'
+
+# perl one liner
+perl -pi -e 's/str_to_find/replace_with_this_str/g' file.txt
+
+# get current external ip
+wget -q -O - checkip.dyndns.org|sed -e 's/.*Current IP Address: //' -e 's/<.*$//'
+
+# sed - on a mac use -i '' otherwise leave it out
+sed -i '' s/str_to_find/replace_with_this_str/g file.txt
+sed s/replacethis/withthis/g < existingfile > newfile
+
+# remove all html tags
+html="<b><div class='classname'>div text</div></b><i>This is some text.</i>"
+sed 's/<[^>]*>//g' <<< "$html"
+sed -e :a -e 's/<[^>]*>//g;/</N;//ba' <<< $html
+
+# recursively search for example.com and replace with frpoc.local
+grep -lr 'example.com' . | xargs sed -i '' 's/example.com/frpoc.local/g'
+
+# useful one-liners
+# http://sed.sourceforge.net/sed1line.txt
+
+# replace comma with new line - outputs to stdout
+tr , '\n' < file.txt
+
+# to upper case
+TEST=xrs
+TEST2=$(echo $TEST | tr '[:lower:]' '[:upper:]')
+
+TEST=xrs
+TEST=$(echo $TEST | tr '[:lower:]' '[:upper:]')
+
+# to lower case
+TEST=xrs
+TEST2=$(echo $TEST | tr '[:upper:]' '[:lower:]')
+
+
